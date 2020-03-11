bf1d89dcb20bd2bdcfb4036a2385421d5546b58c
alisha musamabik <alisha.musamabik@gmail.com>
2020-03-11 20:51:58 +0000

    code challenge2 TTD Practice

--- Coffee Machine/task/src/machine/test/CoffeeCalculateMust.java
+++ Coffee Machine/task/src/machine/test/CoffeeCalculateMust.java
@@ -1,4 +1,22 @@
 package machine.test;
+import machine.CoffeeMachine;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CoffeeCalculateMust {
+    @Test
+    public void returnCoffeeForOne(){
+    // Arrange - we need coffee Calculator of the elements
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+    // Act - should test one line
+        int result = coffeeMachine.calculate(1);
+        //Assert - check the result of the test
+        Assert.assertArrayEquals("1", result);
+
+    }
+
 }
+
+
+
